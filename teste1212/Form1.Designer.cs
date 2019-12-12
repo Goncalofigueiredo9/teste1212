e41c337a9fb07bf4b77a66ea6f02cd487671d4d3
Gonçalo Figueiredo <goncalofigueira53@gmail.com>
2019-12-12 10:57:10 +0000

    Adicionar arquivos de projeto.

---
+++ teste1212/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace teste1212
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.Label();
+            this.n5 = new System.Windows.Forms.Label();
+            this.n4 = new System.Windows.Forms.Label();
+            this.n3 = new System.Windows.Forms.Label();
+            this.n2 = new System.Windows.Forms.Label();
+            this.n6 = new System.Windows.Forms.Label();
+            this.n7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.Location = new System.Drawing.Point(129, 86);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(66, 36);
+            this.n1.TabIndex = 0;
+            this.n1.Text = "Num";
+            // 
+            // n5
+            // 
+            this.n5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.Location = new System.Drawing.Point(473, 86);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(68, 36);
+            this.n5.TabIndex = 1;
+            this.n5.Text = "Num";
+            // 
+            // n4
+            // 
+            this.n4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.Location = new System.Drawing.Point(390, 86);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(59, 36);
+            this.n4.TabIndex = 2;
+            this.n4.Text = "Num";
+            // 
+            // n3
+            // 
+            this.n3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.Location = new System.Drawing.Point(305, 86);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(61, 36);
+            this.n3.TabIndex = 3;
+            this.n3.Text = "Num";
+            // 
+            // n2
+            // 
+            this.n2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.Location = new System.Drawing.Point(219, 86);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(62, 36);
+            this.n2.TabIndex = 4;
+            this.n2.Text = "Num";
+            // 
+            // n6
+            // 
+            this.n6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.Location = new System.Drawing.Point(123, 147);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(48, 36);
+            this.n6.TabIndex = 5;
+            this.n6.Text = "Est";
+            // 
+            // n7
+            // 
+            this.n7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.Location = new System.Drawing.Point(233, 147);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(48, 36);
+            this.n7.TabIndex = 6;
+            this.n7.Text = "Est";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(342, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Sorteio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label n1;
+        private System.Windows.Forms.Label n5;
+        private System.Windows.Forms.Label n4;
+        private System.Windows.Forms.Label n3;
+        private System.Windows.Forms.Label n2;
+        private System.Windows.Forms.Label n6;
+        private System.Windows.Forms.Label n7;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ teste1212/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste1212
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int[] n = new int[5];
+            int[] s = new int[2];
+            for (int i=0;i<5;i++)
+            {
+                bool existe;
+                do
+                {
+                    Random rnd = new Random();
+                    n[i] = rnd.Next(1, 51);
+                    existe = false;
+                    for (int j = 0; j < i; j++)
+                        if (n[i] == n[j]) existe = true;
+                }
+                while (existe);
+            }
+            n1.Text = n[0].ToString();
+            n2.Text = n[1].ToString();
+            n3.Text = n[2].ToString();
+            n4.Text = n[3].ToString();
+            n5.Text = n[4].ToString();
+
+            for (int i = 0; i < 2; i++)
+            {
+                bool existe;
+                do
+                {
+                    Random rnd = new Random();
+                    n[i] = rnd.Next(1, 13);
+                    existe = false;
+                    for (int j = 0; j < i; j++)
+                        if (n[i] == n[j]) existe = true;
+                }
+                while (existe);
+            }
+            n6.Text = n[0].ToString();
+            n7.Text = n[1].ToString();
+
+        }
+    }
+}
